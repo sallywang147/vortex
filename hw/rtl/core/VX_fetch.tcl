b1396c54cd5fc01bcf7891a6eae5b29dea3ba59d
sallywang147 <sallyjunsongwang@gmail.com>
2024-09-13 10:00:46 +0000

    no colon

--- hw/rtl/core/VX_fetch.tcl
+++ hw/rtl/core/VX_fetch.tcl
@@ -46,7 +46,7 @@ assert -name fetch_if_ready {
 
 # Cover: A complete fetch cycle occurs
 cover -name complete_fetch_cycle {
-    sequence complete_fetch;
+    sequence complete_fetch
         !reset && schedule_if.valid && schedule_if.ready ##1
         icache_bus_if.req_valid && icache_bus_if.req_ready ##1
         icache_bus_if.rsp_valid && icache_bus_if.rsp_ready ##1
